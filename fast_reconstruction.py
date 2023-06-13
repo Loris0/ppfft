83e2e5998270d590a9298270d9de9e847be36a05
Loris Megy <loris.megy@esrf.fr>
2023-06-13 14:43:37 +0200

    Added 2 step 1D interpolation method for polar to pseudopolar

--- fast_reconstruction.py
+++ fast_reconstruction.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 from pad import pad
-from polar_to_pp import direct_2d_interp
+from polar_to_pp import direct_2d_interp, polar_to_pseudopolar, new_polar_to_pseudopolar
 from fast_direct_inverse import fast_direct_inversion
 from iterative_inverse import iterative_inverse
 
 
-def fast_reconstruction(sino, precomputations, angles=None, tol=1e-3):
+def fast_reconstruction(sino, precomputations, interp_mode="1d", angles=None, tol=1e-3):
     """
     The angles of the projections are supposed to be equispaced in [-pi/2, pi/2[
     """
@@ -18,15 +18,30 @@ def fast_reconstruction(sino, precomputations, angles=None, tol=1e-3):
         np.fft.fft(np.fft.ifftshift(pad_sino, axes=-1)), axes=-1
     )
 
-    if angles is None:
-        angles = np.linspace(-np.pi / 2, np.pi / 2, num=n_theta, endpoint=False)
+    if interp_mode == "1d":
+        if n_theta == n:
+            hori_ppfft, vert_ppfft = new_polar_to_pseudopolar(fft_sinogram.T)
 
-    p = np.arange(-n, n + 1)
+        elif n_theta == 2 * n:
+            hori_ppfft, vert_ppfft = polar_to_pseudopolar(fft_sinogram.T)
 
-    x_polar = p[None, :] * np.cos(np.pi / 2 + angles)[:, None]
-    y_polar = p[None, :] * np.sin(np.pi / 2 + angles)[:, None]
+        else:
+            raise ValueError(
+                "The number of projections must be equal to n or 2n in order to use the 1d interpolation mode."
+            )
 
-    hori_ppfft, vert_ppfft = direct_2d_interp(fft_sinogram, x_polar, y_polar, n)
+    # else, use direct 2d interpolation (slow)
+    else:
+        # if angles is not provided, assume they are equispaced in [0, \pi[
+        if angles is None:
+            angles = np.linspace(-np.pi / 2, np.pi / 2, num=n_theta, endpoint=False)
+
+        p = np.arange(-n, n + 1)
+
+        x_polar = p[None, :] * np.cos(np.pi / 2 + angles)[:, None]
+        y_polar = p[None, :] * np.sin(np.pi / 2 + angles)[:, None]
+
+        hori_ppfft, vert_ppfft = direct_2d_interp(fft_sinogram, x_polar, y_polar, n)
 
     if tol is None:
         return fast_direct_inversion(hori_ppfft, vert_ppfft, precomputations)
--- polar_to_pp.py
+++ polar_to_pp.py
@@ -2,6 +2,11 @@ import numpy as np
 from scipy.interpolate import interp1d, CloughTocher2DInterpolator
 
 
+"""
+Grid definitions.
+"""
+
+
 def horizontal_lines(n: int):
     coords = np.empty(shape=(n + 1, 2 * n + 1, 2))
 
@@ -50,7 +55,12 @@ def polar_fourier_transform(im):
     return res
 
 
-def interpolate_vert_ray(q, ray_samples, kind="linear"):
+"""
+Interpolation in two steps, when n_theta = 2n.
+"""
+
+
+def interpolate_vert_ray(q, ray_samples, kind="cubic"):
     """
     Interpolate rays corresponding to the vertical PPFFT.
     0 <= q <= n/2 or n + n/2 <= q <= 2n - 1
@@ -76,7 +86,7 @@ def interpolate_vert_ray(q, ray_samples, kind="linear"):
     return res
 
 
-def interpolate_all_vert_rays(polar_ft, kind="linear"):
+def interpolate_all_vert_rays(polar_ft, kind="cubic"):
     """
     Computes the first step towards the vertical PPFFT.
     """
@@ -99,7 +109,7 @@ def interpolate_all_vert_rays(polar_ft, kind="linear"):
     return res[::, ::-1]
 
 
-def interpolate_hori_ray(q, ray_samples, kind="linear"):
+def interpolate_hori_ray(q, ray_samples, kind="cubic"):
     """
     Interpolate rays corresponding to the horizontal PPFFT.
     n/2 <= q <= n + n/2
@@ -118,7 +128,7 @@ def interpolate_hori_ray(q, ray_samples, kind="linear"):
     return interpolator(target_pos)
 
 
-def interpolate_all_hori_rays(polar_ft, kind="linear"):
+def interpolate_all_hori_rays(polar_ft, kind="cubic"):
     """
     Computes the first step towards the horizontal PPFFT.
     """
@@ -142,7 +152,7 @@ def interpolate_all_hori_rays(polar_ft, kind="linear"):
     return res
 
 
-def interpolate_vert_angle(k, samples, kind="linear"):
+def interpolate_vert_angle(k, samples, kind="cubic"):
     """
     Reconstruct one column of the horizontal PPFFT.
     -n <= k <= n
@@ -172,7 +182,7 @@ def interpolate_vert_angle(k, samples, kind="linear"):
     return res
 
 
-def interpolate_hori_angle(k, samples, kind="linear"):
+def interpolate_hori_angle(k, samples, kind="cubic"):
     """
     Reconstruct one line of the horizontal PPFFT.
     -n <= k <= n
@@ -199,7 +209,7 @@ def interpolate_hori_angle(k, samples, kind="linear"):
     return res
 
 
-def interpolate_all_vert_angles(vert_rays, kind="linear"):
+def interpolate_all_vert_angles(vert_rays, kind="cubic"):
     """
     ``vert_rays`` is the ouput of ``interpolate_all_vert_rays``.
     Its shape is (2n+1, n+1)
@@ -217,7 +227,7 @@ def interpolate_all_vert_angles(vert_rays, kind="linear"):
     return vert_ppfft.T
 
 
-def interpolate_all_hori_angles(hori_rays, kind="linear"):
+def interpolate_all_hori_angles(hori_rays, kind="cubic"):
     """
     ``hori_rays`` is the ouput of ``interpolate_all_hori_rays``.
     Its shape is (2n+1, n+1)
@@ -235,13 +245,18 @@ def interpolate_all_hori_angles(hori_rays, kind="linear"):
     return hori_ppfft.T
 
 
-def polar_to_pseudopolar(polar_ft, kind="linear"):
+def polar_to_pseudopolar(polar_ft, kind="cubic"):
     vert_rays = interpolate_all_vert_rays(polar_ft, kind)
     hori_rays = interpolate_all_hori_rays(polar_ft, kind)
     vert_ppfft = interpolate_all_vert_angles(vert_rays, kind)
     hori_ppfft = interpolate_all_hori_angles(hori_rays, kind)
 
     return hori_ppfft, vert_ppfft
+
+
+"""
+Direct 2D interpolation (very slow)
+"""
 
 
 def direct_2d_interp(polar_ft, x, y, n, interp_fun=CloughTocher2DInterpolator):
@@ -274,5 +289,212 @@ def direct_2d_interp(polar_ft, x, y, n, interp_fun=CloughTocher2DInterpolator):
 
     hori_ppfft = interpolator(hori_pos[..., 0], hori_pos[..., 1])
     vert_ppfft = interpolator(vert_pos[..., 0], vert_pos[..., 1])
+
+    return hori_ppfft, vert_ppfft
+
+
+"""
+Interpolation in two steps, when n_theta = n
+"""
+
+
+def new_interpolate_vert_ray(q, ray_samples, kind="cubic"):
+    """
+    Interpolate rays corresponding to the vertical PPFFT.
+    0 <= q <= n // 4 or n - n // 4 <= q <= n - 1
+
+    ``ray_samples`` always goes from the bottom of the circle to the top.
+    """
+    n = (len(ray_samples) - 1) // 2
+
+    ray_pos = np.arange(-n, n + 1)  # positions of the known points along the line
+    theta = np.pi * q / n
+    target_pos = np.arange(-n, n + 1) * np.sqrt(1 + np.tan(theta) ** 2)
+
+    # Interpolate and put zeros outside of the polar disk
+    interpolator = interp1d(
+        ray_pos, ray_samples, bounds_error=False, fill_value=0, kind=kind
+    )
+
+    if 0 <= q <= n // 4:
+        res = interpolator(target_pos)
+    else:
+        res = interpolator(target_pos)[::-1]
+
+    return res
+
+
+def new_interpolate_all_vert_rays(polar_ft, kind="cubic"):
+    """
+    Computes the first step towards the vertical PPFFT.
+
+    polar_ft has shape: (2n + 1, n)
+    """
+    n = np.shape(polar_ft)[1]
+    q_n, r_n = divmod(n, 4)
+
+    res = np.zeros(shape=(2 * n + 1, 2 * q_n + 1), dtype=complex)
+
+    i = 0
+    for q in range(n - q_n, n):
+        res[:, i] = new_interpolate_vert_ray(q, polar_ft[:, q], kind)
+        i += 1
+
+    res[:, i] = polar_ft[:, 0]  # theta = 0, horizontal coordinates
+    i += 1
+
+    for q in range(1, q_n + 1):
+        res[:, i] = new_interpolate_vert_ray(q, polar_ft[:, q], kind)
+        i += 1
+
+    return res[::, ::-1]
+
+
+def new_interpolate_hori_ray(q, ray_samples, kind="cubic"):
+    """
+    Interpolate rays corresponding to the horizontal PPFFT.
+    n // 4 + ((n % 4) != 0) <= q <= n - n // 4 - ((n % 4) != 0)
+    """
+    n = (len(ray_samples) - 1) // 2
+
+    ray_pos = np.arange(-n, n + 1)  # positions of the known points along the line
+    theta = np.pi * q / n
+    target_pos = np.arange(-n, n + 1) * np.sqrt(1 + 1 / np.tan(theta) ** 2)
+
+    # Interpolate and put zeros outside of the polar disk
+    interpolator = interp1d(
+        ray_pos, ray_samples, bounds_error=False, fill_value=0, kind=kind
+    )
+
+    return interpolator(target_pos)
+
+
+def new_interpolate_all_hori_rays(polar_ft, kind="cubic"):
+    """
+    Computes the first step towards the horizontal PPFFT.
+    """
+    n = np.shape(polar_ft)[1]
+    q_n, r_n = divmod(n, 4)
+
+    res = np.zeros(shape=(2 * n + 1, 2 * q_n + 1), dtype=complex)
+
+    i = 0
+
+    for q in range(n // 4 + (r_n != 0), n // 2):
+        res[:, i] = new_interpolate_hori_ray(q, polar_ft[:, q], kind)
+        i += 1
+
+    res[:, i] = polar_ft[:, n // 2]
+    i += 1
+
+    for q in range(n // 2 + 1, n - n // 4 - (r_n != 0) + 1):
+        res[:, i] = new_interpolate_hori_ray(q, polar_ft[:, q], kind)
+        i += 1
+
+    return res
+
+
+def new_interpolate_vert_angle(k, n, samples, kind="cubic"):
+    """
+    Reconstruct one column of the horizontal PPFFT.
+    -n <= k <= n
+    """
+    q_n, r_n = divmod(n, 4)
+
+    # samples at x = k
+    # and angles pi * q / 2n with q = n/2, ... 0, 2n-1, ..., n + n/2
+    # meaning the y positions are: x_q = k * tan(theta_q)
+    q = np.concatenate((np.arange(0, q_n + 1)[::-1], np.arange(n - q_n, n)[::-1]))
+    samples_pos = k * np.tan(np.pi * q / n)
+
+    # Initialize interpolator
+    interpolator = interp1d(
+        samples_pos, samples, bounds_error=False, fill_value=0, kind=kind
+    )
+
+    # The target y positions are: y_l = -2lk / n with -n/2 <= l <= n/2
+    target_pos = np.arange(-(n // 2), n // 2 + 1) * (-2 * k / n)
+
+    # Compute result
+    res = np.zeros_like(target_pos, dtype=complex)
+    res[1:-1] = interpolator(target_pos[1:-1])
+    res[0] = samples[0]
+    res[-1] = samples[-1]
+
+    return res
+
+
+def new_interpolate_hori_angle(k, n, samples, kind="cubic"):
+    """
+    Reconstruct one line of the horizontal PPFFT.
+    -n <= k <= n
+    """
+    q_n, r_n = divmod(n, 4)
+
+    # samples at y = k
+    # and angles pi * q / 2n with n/2 <= q <= n + n/2
+    # meaning the x positions are: x_q = k / tan(theta_q)
+    samples_pos = k / np.tan(
+        np.pi * np.arange(q_n + (r_n != 0), n - q_n - (r_n != 0) + 1) / n
+    )
+
+    # Initialize interpolator
+    interpolator = interp1d(
+        samples_pos, samples, bounds_error=False, fill_value=0, kind=kind
+    )
+
+    # The target x positions are: x_l = -2lk / n with -n/2 <= l <= n/2
+    target_pos = np.arange(-(n // 2), n // 2 + 1) * (-2 * k / n)
+
+    # Compute result
+    res = np.zeros_like(target_pos, dtype=complex)
+    res[0] = samples[0]  # the first point is already known
+    res[-1] = samples[-1]  # the last one too
+    res[1:-1] = interpolator(target_pos[1:-1])
+
+    return res
+
+
+def new_interpolate_all_vert_angles(vert_rays, kind="cubic"):
+    """
+    ``vert_rays`` is the ouput of ``interpolate_all_vert_rays``.
+    Its shape is (2n+1, 2 * (n // 4) + 1)
+    """
+    n = np.shape(vert_rays)[0] // 2
+    vert_ppfft = np.zeros(shape=(2 * n + 1, n + 1), dtype=complex)
+
+    for k in np.arange(-n, n + 1):
+        if k != 0:
+            samples = vert_rays[k + n]
+            vert_ppfft[k + n] = new_interpolate_vert_angle(k, n, samples, kind)
+        else:
+            vert_ppfft[k + n] = vert_rays[k + n, n // 4]
+
+    return vert_ppfft.T
+
+
+def new_interpolate_all_hori_angles(hori_rays, kind="cubic"):
+    """
+    ``hori_rays`` is the ouput of ``interpolate_all_hori_rays``.
+    Its shape is (2n+1, 2 * (n // 4) + 1)
+    """
+    n = np.shape(hori_rays)[0] // 2
+    hori_ppfft = np.zeros(shape=(2 * n + 1, n + 1), dtype=complex)
+
+    for k in np.arange(-n, n + 1):
+        if k != 0:
+            samples = hori_rays[k + n]
+            hori_ppfft[k + n] = new_interpolate_vert_angle(k, n, samples, kind)
+        else:
+            hori_ppfft[k + n] = hori_rays[k + n, n // 4]
+
+    return hori_ppfft.T
+
+
+def new_polar_to_pseudopolar(polar_ft, kind="cubic"):
+    vert_rays = new_interpolate_all_vert_rays(polar_ft, kind)
+    hori_rays = new_interpolate_all_hori_rays(polar_ft, kind)
+    vert_ppfft = new_interpolate_all_vert_angles(vert_rays, kind)
+    hori_ppfft = new_interpolate_all_hori_angles(hori_rays, kind)
 
     return hori_ppfft, vert_ppfft

0039023d121286fe989c0cfb47c9750037d7dd88
Loris Megy <loris.megy@esrf.fr>
2023-06-23 16:33:39 +0200

    Docstrings for ppfft/ppfft

---
+++ src/ppfft/ppfft/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module containing the definition of the 
+Pseudo-Polar Fast Fourier Transform and its adjoint.
+"""
+
+from .ppfft import adj_ppfft, ppfft
--- src/ppfft/ppfft/ppfft.py
+++ src/ppfft/ppfft/ppfft.py
@@ -14,14 +14,15 @@ from ..tools.frac_fft import frac_fft_for_ppfft, adj_frac_fft_for_ppfft
 
 
 def ppfft_horizontal(a: np.ndarray) -> np.ndarray:
-    """
-    Pseudo-Polar Fast Fourier Transform on the basically horizontal lines.
+    """Pseudo-Polar Fast Fourier Transform on the basically horizontal lines.
 
-    ## Parameters
+    Parameters
+    ----------
     a : np.ndarray
-        Input array of shape (n, n) where n is even.
+        Input array of shape (n, n).
 
-    ## Returns
+    Returns
+    -------
     y : np.ndarray
         Ouput array of shape (n+1, 2n+1).
     """
@@ -39,32 +40,35 @@ def ppfft_horizontal(a: np.ndarray) -> np.ndarray:
 
 
 def ppfft_vertical(a: np.ndarray) -> np.ndarray:
-    """
-    Pseudo-Polar Fast Fourier Transform on the basically vertical lines.
+    """Pseudo-Polar Fast Fourier Transform on the basically vertical lines.
 
-    ## Parameters
+    Parameters
+    ----------
     a : np.ndarray
-        Input array of shape (n, n) where n is even.
+        Input array of shape (n, n).
 
-    ## Returns
+    Returns
+    -------
     y : np.ndarray
         Ouput array of shape (n+1, 2n+1).
 
-    ## See Also
-    ppfft_horizontal : Return the PPFFT on the basically horizontal lines.
+    See Also
+    --------
+    ppfft_horizontal : PPFFT on the basically horizontal lines.
     """
     return ppfft_horizontal(a.T)
 
 
 def ppfft(a: np.ndarray):
-    """
-    Pseudo-Polar Fast Fourier Transform.
+    """Pseudo-Polar Fast Fourier Transform.
 
-    ## Parameters
+    Parameters
+    ----------
     a : np.ndarray
-        Input array of shape (n, n) where n is even.
+        Input array of shape (n, n).
 
-    ## Returns
+    Returns
+    -------
     hori : np.ndarray
         Horizontal ppfft of shape (n+1, 2n+1)
     vert : np.ndarray
@@ -74,6 +78,20 @@ def ppfft(a: np.ndarray):
 
 
 def adj_ppfft(hori_ppfft: np.ndarray, vert_ppfft: np.ndarray) -> np.ndarray:
+    """Adjoint of `ppfft`.
+
+    Parameters
+    ----------
+    hori_ppfft : np.ndarray
+        Horizontal PPFFT.
+    vert_ppfft : np.ndarray
+        Vertical PPFFT.
+
+    Returns
+    -------
+    out : np.ndarray
+        Adjoint PPFFT applied to input.
+    """
     n, m = hori_ppfft.shape[0] - 1, hori_ppfft.shape[1]
     hori_aux = np.empty(shape=(n, m), dtype=complex)
     vert_aux = np.empty(shape=(n, m), dtype=complex)

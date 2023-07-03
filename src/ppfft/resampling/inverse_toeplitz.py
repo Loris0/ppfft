28951dbc9bb1f37d3f328fd2b66bd61e55aafc40
Loris Megy <loris.megy@esrf.fr>
2023-07-03 14:17:27 +0200

    Faster version of apply_inverse

--- src/ppfft/resampling/inverse_toeplitz.py
+++ src/ppfft/resampling/inverse_toeplitz.py
@@ -4,7 +4,7 @@ the fast resampling of trigonometric polynomials.
 """
 
 import numpy as np
-from scipy.linalg import solve_toeplitz, matmul_toeplitz
+from scipy.linalg import solve_toeplitz
 
 
 class InverseToeplitz:
@@ -18,7 +18,7 @@ class InverseToeplitz:
     """
 
     def __init__(self, col: np.ndarray, row=None) -> None:
-        """A Toeplitz matrix T is represented by:
+        """Constructor of the class.:
 
         Parameters
         ----------
@@ -27,25 +27,24 @@ class InverseToeplitz:
         row : np.ndarray | None
             First row of T. By default None, meaning row = conj(col)
 
-
-        - its first column: col
-        - its first row: row
-
-        If row is not provided, it is assumed T is Hermitian.
         """
         self.col = col
         self.row = row
+        self.n = len(col)
 
         self.x0 = None
-        self.m1 = None
-        self.m2 = None
-        self.m3 = None
-        self.m4 = None
+        self.fft_m1 = None
+        self.fft_m2 = None
+        self.fft_m3 = None
+        self.fft_m4 = None
 
         self.gohberg_semencul()
 
     def gohberg_semencul(self) -> None:
-        """Computes the Gohberg-Semencul decomposition of T^{-1}"""
+        """Computes the Gohberg-Semencul decomposition of T^{-1}
+
+        We only store the FFT of the corresponding convolution kernels.
+        """
         e0 = np.zeros_like(self.col)
         e0[0] = 1
 
@@ -60,45 +59,44 @@ class InverseToeplitz:
             x = solve_toeplitz((self.col, self.row), e0)
             y = solve_toeplitz((self.col, self.row), e1)
 
-        x_a = np.zeros_like(x)
-        x_a[0] = x[0]
+        self.fft_m1 = np.fft.fft(np.concatenate((np.zeros(self.n - 1), x)))
 
-        x_b = np.zeros_like(x)
-        x_b[1::] = x[:0:-1]
+        self.fft_m4 = np.fft.fft(np.concatenate((x[1:], np.zeros(self.n))))
 
-        y_a = np.zeros_like(y)
-        y_a[0] = y[-1]
+        col_m2 = np.zeros_like(y)
+        col_m2[0] = y[-1]
+        self.fft_m2 = np.fft.fft(np.concatenate((y[:-1], col_m2)))
 
-        y_b = np.zeros_like(y)
-        y_b[1::] = y[:-1]
+        col_m3 = np.zeros_like(y)
+        col_m3[1::] = y[:-1]
+        self.fft_m3 = np.fft.fft(np.concatenate((np.zeros(self.n - 1), col_m3)))
 
         self.x0 = x[0]
-        self.m1 = (x, x_a)
-        self.m2 = (y_a, y[::-1])
-        self.m3 = (y_b, np.zeros_like(y))
-        self.m4 = (np.zeros_like(x), x_b)
 
-    def apply_inverse(self, vec: np.ndarray, workers: int = 8) -> np.ndarray:
+    def apply_inverse(self, x: np.ndarray) -> np.ndarray:
         """Computes T^{-1} @ vec using the Gohberg-Semencul formula.
 
         Parameters
         ----------
-        vec : np.ndarray
-            Vector to compute the product T^{-1} @ vec.
+        x : np.ndarray
+            Vector to compute the product T^{-1} @ x.
         workers : int
             Workers parameter for scipy.linalg.matmul_toeplitz, by default 8
 
         Returns
         -------
         out : np.ndarray
-            Value of T^{-1} @ vec
+            Value of T^{-1} @ x
         """
-        M1M2_v = matmul_toeplitz(
-            self.m1, matmul_toeplitz(self.m2, vec, workers), workers
-        )
 
-        M3M4_v = matmul_toeplitz(
-            self.m3, matmul_toeplitz(self.m4, vec, workers), workers
-        )
+        fft_x = np.fft.fft(x, n=2 * self.n - 1)
 
-        return (M1M2_v - M3M4_v) / self.x0
+        m2_x = np.fft.ifft(self.fft_m2 * fft_x)[-self.n :]
+        m4_x = np.fft.ifft(self.fft_m4 * fft_x)[-self.n :]
+
+        res = np.fft.ifft(
+            self.fft_m1 * np.fft.fft(m2_x, n=2 * self.n - 1)
+            - self.fft_m3 * np.fft.fft(m4_x, n=2 * self.n - 1)
+        )[-self.n :]
+
+        return res / self.x0
